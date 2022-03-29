32f7034a26257141909f5b2cfdc2227fcbfb59e4
Vincent Hu <vincent.guohua.hu@gmail.com>
2022-03-29 21:31:29 +0800

    add User model, and set up database

--- apps/assistant/settings.py
+++ apps/assistant/settings.py
@@ -15,10 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-qra)2=gbu)4=y^ia3y9g33l@1+^v!byl^-*=%+6h8aka83b5hx'
 
@@ -75,12 +71,15 @@ WSGI_APPLICATION = 'assistant.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'HomeAssistant.db',
+				#'USER': '',
+				#'PASSWORD': '',
+				#'HOST': '',
+				#'PORT': '',
     }
 }
 
--- apps/portal/models.py
+++ apps/portal/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+		"""
+		Class definition of User
+		"""
+		GENDER_CHOICES = [
+				('M', 'Male'),
+				('F', 'Female'),
+				('U', 'Unknown'),
+		]
+
+		first_name = models.CharField(max_length=30)
+		last_name = models.CharField(max_length=30)
+		age = models.IntegerField(null=True, blank=True)
+		gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='U')
+		address = models.CharField(null=True, blank=True, max_length=50)
+		email = models.EmailField(null=True, blank=True)
+		phone = models.CharField(null=True, blank=True, max_length=11)
+		is_superuser = models.BooleanField(default=False)
+		create_date = models.DateTimeField(auto_now_add=True)
+		last_login = models.DateField(null=True, blank=True)
+		login_ip = models.GenericIPAddressField(null=True, blank=True)
