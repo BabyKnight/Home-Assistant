57a79b66ed44952073b4d15cad1c882f89108cf5
Vincent Hu <vincent.guohua.hu@gmail.com>
2022-03-31 21:38:06 +0800

    add template for welcome page and update views

--- apps/portal/views.py
+++ apps/portal/views.py
@@ -3,10 +3,12 @@ from django.http import HttpResponse
 
 
 def welcome(request):
-		return HttpResponse("Welocme Home!")
+		#return HttpResponse("Welocme Home!")
+		template_name = 'welcome.html'
+		return render(request, template_name)
 
 def portal(request, lang='en'):
-		return HttpResponse("[{}] Welcome Home! This is The portal page of Home-Assistant".format(lang))
+		return HttpResponse("[%s] Welcome Home! This is The portal page of Home-Assistant" % lang)
 
 def login(request):
 		return HttpResponse("Login page")
