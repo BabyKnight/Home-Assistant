fbf7ef8ac04bc9f7a1cd9a6a1eea865609cf1c1d
Vincent Hu <vincent.guohua.hu@gmail.com>
2022-04-02 14:03:10 +0800

    add csrf token in the template

--- apps/home/views.py
+++ apps/home/views.py
@@ -11,5 +11,6 @@ def portal(request, lang='en'):
 		return HttpResponse("[%s] Welcome Home! This is The portal page of Home-Assistant" % lang)
 
 def login(request):
+		print(request.method)
 		template_name = 'login.html'
 		return render(request, template_name)
--- apps/project/urls.py
+++ apps/project/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-		# resirect all request to the portal app except admin
+		# resirect all request to the home app except admin
     path('', include('home.urls')),
     path('admin/', admin.site.urls),
 ]
