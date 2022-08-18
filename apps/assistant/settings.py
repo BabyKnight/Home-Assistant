913e1832fe967366267a8a5eb50ec866ff768898
Vincent Hu <vincent.guohua.hu@gmail.com>
2022-08-18 15:46:55 +0800

    add DB settings for heroku

--- apps/assistant/settings.py
+++ apps/assistant/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,6 +83,9 @@ DATABASES = {
 				#'PORT': '',
     }
 }
+
+# add DB settings for heroku
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
