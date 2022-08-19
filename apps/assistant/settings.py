366b4705da84a252ec716de6ce66bbad113d5d9c
Vincent Hu <vincent.guohua.hu@gmail.com>
2022-08-19 10:34:20 +0800

    update staticfile directory

--- apps/assistant/settings.py
+++ apps/assistant/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from os.path import join
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,11 +123,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-#STATICFILES_DIRS = [
-#		BASE_DIR / "static",
-#]
+STATICFILES_DIRS = [
+		join(BASE_DIR,'static'),
+]
 
 # Enable WhiteNoise's GZip compression of static assets.
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
