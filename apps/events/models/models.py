c251c3dd2040e9ffca88434e1fae29a12dc4b512
Vincent Hu <vincent.guohua.hu@gmail.com>
2023-02-07 22:45:24 +0800

    add basic events models

--- apps/events/models/models.py
+++ apps/events/models/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+		"""
+		Class definition of Event
+		"""
+		CATEGORY_CHOICES = [
+				('A', 'Anniversary'),
+				('B', 'Birthday'),
+				('P', 'Package'),
+				('U', 'Undefined'),
+		]
+		STATUS_CHOICES = [
+				('T', 'TODO'),
+				('I', 'IN PROGRESS'),
+				('D', 'DONE'),
+		]
+		category = models.CharField(max_length=3, blank=False, null=False, choices=CATEGORY_CHOICES, default='U')
+		detail = models.CharField(null=True, blank=True, max_length=50)
+		location = models.CharField(null=True, blank=True, max_length=50)
+		status = models.CharField(max_length=3, blank=False, null=False, choices=STATUS_CHOICES, default='T')
+		time = models.DateTimeField(null=True, blank=True)
+		tag = models.CharField(null=True, blank=True, max_length=30)
+		
+		class Meta:
+			verbose_name = 'Event'
+
+		def __str__(self):
+			return "<{}, {}>".format(
+					self.category,
+					self.detail[:20],
+					)
